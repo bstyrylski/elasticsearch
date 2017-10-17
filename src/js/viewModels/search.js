1f9fdc7c0e14942b88c51ebb8a7ad6ffb6e828f7
Bartek Styrylski <bartek.styrylski@oracle.com>
2017-10-17 15:47:08 +0200

    Progress on seach

--- src/js/viewModels/search.js
+++ src/js/viewModels/search.js
@@ -15,33 +15,39 @@ define(['ojs/ojcore', 'knockout', 'ojs/ojinputtext', 'ojs/ojtable', 'ojs/ojarray
     return function searchContentViewModel() {
         var self = this;
 
-        self.searchPhrase = ko.observable("Manager");
+        self.searchPhrase = ko.observable("");
         self.jobs = ko.observableArray();
         
-        var payload = {
-            query: {
-                bool: {
-                    must: [{
-                            multi_match: {
-                                query: self.searchPhrase(),
-                                fields: ["JobCode", "Name", "JobFamilyName", "JobFunctionCode"]
+        self.searchButtonClick = function(event) {
+            var payload = {
+                query: {
+                    bool: {
+                        must: [{
+                                multi_match: {
+                                    query: self.searchPhrase(),
+                                    fields: ["JobCode", "Name", "JobFamilyName", "JobFunctionCode"]
+                                }
                             }
-                        }
-                    ]
+                        ]
+                    }
                 }
-            }
-        };
+            };
 
-        $.post("http://localhost:1337/slc12qen.us.oracle.com:9200/jobs/_search", JSON.stringify(payload))
-            .done(function (searchResult) {
-                $.each(searchResult.hits.hits, function () {
-                    self.jobs.push({
-                        id: this._id,
-                        score: this._score
+            $.post("http://localhost:1337/slc12qen.us.oracle.com:9200/jobs/_search", JSON.stringify(payload))
+                .done(function (searchResult) {
+                    self.jobs([]);
+                    
+                    $.each(searchResult.hits.hits, function () {
+                        self.jobs.push({
+                            jobCode: this._source.JobCode,
+                            jobName: this._source.Name,
+                            jobFamilyName: this._source.JobFamilyName,
+                            jobFunctionCode: this._source.JobFunctionCode
+                        });
                     });
                 });
-            });
+        }
 
-        self.results = new oj.ArrayTableDataSource(self.jobs, {idAttribute: "id"});
+        self.results = new oj.ArrayTableDataSource(self.jobs, {idAttribute: "jobCode"});
     }
 });
