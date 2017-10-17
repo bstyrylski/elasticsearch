b94031e2dc2863357fa6b8b09613455ec470793c
Bartek Styrylski <bartek.styrylski@oracle.com>
2017-10-17 15:17:40 +0200

    Progress on search

--- src/js/viewModels/search.js
+++ src/js/viewModels/search.js
@@ -7,14 +7,39 @@
 /**
  * search module
  */
-define(['ojs/ojcore', 'knockout'
+define(['ojs/ojcore', 'knockout', 'ojs/ojinputtext', 'ojs/ojtable', 'ojs/ojarraytabledatasource'
 ], function (oj, ko) {
     /**
      * The view model for the main content view template
      */
-    function searchContentViewModel() {
+    return function searchContentViewModel() {
         var self = this;
+
+        self.searchPhrase = ko.observable("");
+        self.jobs = ko.observableArray();
+
+        $.post("http://localhost:1337/slc12qen.us.oracle.com:9200/jobs/_search", JSON.stringify({
+            query: {
+                bool: {
+                    must: [{
+                            multi_match: {
+                                query: "Manager",
+                                fields: ["JobCode", "Name", "JobFamilyName", "JobFunctionCode"]
+                            }
+                        }
+                    ]
+                }
+            }
+        }))
+                .done(function (searchResult) {
+                    $.each(searchResult.hits.hits, function () {
+                        self.jobs.push({
+                            id: this._id,
+                            score: this._score
+                        });
+                    });
+                });
+
+        self.results = new oj.ArrayTableDataSource(self.jobs, {idAttribute: "id"});
     }
-    
-    return searchContentViewModel;
 });
